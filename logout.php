3be7dfe55e7cf3df2a6d11f12740bb0890013ec7
Your Name <sutianer0000@gmail.com>
2026-04-25 14:35:29 +0700

    fix: clear session cookie on logout

--- logout.php
+++ logout.php
@@ -5,13 +5,26 @@ $_SESSION = [];
 
 if (ini_get('session.use_cookies')) {
     $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000, [
-        'path' => $params['path'],
-        'domain' => $params['domain'],
-        'secure' => $params['secure'],
-        'httponly' => $params['httponly'],
-        'samesite' => $params['samesite'] ?? 'Lax',
-    ]);
+    if (PHP_VERSION_ID >= 70300) {
+        setcookie(session_name(), '', [
+            'expires' => time() - 42000,
+            'path' => $params['path'] ?: '/',
+            'domain' => $params['domain'] ?? '',
+            'secure' => (bool) $params['secure'],
+            'httponly' => (bool) $params['httponly'],
+            'samesite' => $params['samesite'] ?? 'Lax',
+        ]);
+    } else {
+        setcookie(
+            session_name(),
+            '',
+            time() - 42000,
+            $params['path'] ?: '/',
+            $params['domain'] ?? '',
+            (bool) $params['secure'],
+            (bool) $params['httponly']
+        );
+    }
 }
 
 session_destroy();
